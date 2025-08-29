ad00903aad941290bcf296b14afd2773f0a29514
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-29 10:05:44 +0300

    Userauths

--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -6,4 +6,12 @@ from django.contrib.auth.models import AbstractBaseUser
 class User(AbstractBaseUser):
     username = models.CharField(unique=True, max_length=100)
     email = models.EmailField(unique=True)
+    full_name= models.CharField(unique=True, max_length=100)
+    otp = models.CharField(unique=True, max_length=100)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
 
