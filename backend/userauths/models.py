2736e2be945cce3f414a70bca9cf1b47ad802bb9
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-29 10:02:51 +0300

    Userauths

--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -4,4 +4,5 @@ from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
 
 class User(AbstractBaseUser):
-    username = models.CharField(unique=True)
+    username = models.CharField(unique=True, max_length=100)
+
