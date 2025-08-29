f468f7552d1cbecd715aeca0d067dfbce6802d77
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-29 10:20:10 +0300

    Userauths

--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -37,5 +37,14 @@ class Profile(models.Model):
             return str(self.full_name)
         else:
             return str(self.user.full_name)
+        
+    def save(self, *args, **kwargs):
+        email_username, full_name = self.email.split('@')
+        if self.full_name == "" or self.full_name == None:
+            self.full_name == email_username
+        
+        if self.username == "" or self.username == None:
+            self.username == email_username
+        super(User, self).save(*args, **kwargs)
 
 
