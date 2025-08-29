816655a1c0b070224c958eb54f8f2dfbbf1f3ee8
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-29 09:32:41 +0300

    Customizing static and media files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,6 +123,8 @@ STATIC_URL = 'static/'
 
 STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+STATIC_ROOTS = BASE_DIR /'templates'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
