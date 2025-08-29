cc7345e098398faaf8c0425e3a06cf6bf84baa83
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-29 09:41:56 +0300

    Backend urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,3 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
