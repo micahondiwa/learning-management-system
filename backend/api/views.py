6e877408966281377cbda89709d3890e7a63319d
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-30 09:42:01 +0300

    Password reset email verificatin

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
 from userauths.models import User, Profile
 from rest_framework.permissions import AllowAny
+import random
 
 # Create your views here.
 class MyTokenObtainPairView(TokenObtainPairView):
@@ -14,6 +15,9 @@ class RegisterView(generics.CreateAPIView):
     Permission_class = [AllowAny]
     serializer_class = api_serializer.RegistrationSerializer
 
+def generate_random_otp(length=7):
+    otp = ''.join([str(random.randint(0, 9)) for _ in range(length)])
+
 class PasswordResetEmailVerifyAPIView(generics.RetrieveAPIView):
     Permission_class = [AllowAny]
     serializer_class = api_serializer.UserSerializer
@@ -21,4 +25,7 @@ class PasswordResetEmailVerifyAPIView(generics.RetrieveAPIView):
     def get_object(self):
         email = self.kwargs['email']
 
-        user = User.objects.get(email=email)
+        user = User.objects.filter(email=email).first()
+
+        if user:
+            user.objects.otp = 
