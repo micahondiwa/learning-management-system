55e64917706658ca097c6b36dfd69f3cc0e3da6f
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-30 14:01:13 +0300

    Axios instance

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,5 +2,9 @@ import axios from "axios";
 
 const apiInstance = axios.create({
     baseURL: 'http://127.0.0.1:8000/api/v1/',
-    timeout: 5000
+    timeout: 5000,
+    headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+    }
 })
