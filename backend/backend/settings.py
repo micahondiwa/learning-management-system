7a64ebc6940d79c900cc0b6455a2b8e4ea025646
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-30 07:18:00 +0300

    Custom user

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,6 +128,8 @@ STATIC_ROOTS = BASE_DIR / 'templates'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTH_USER_MODEL ='userauths.User'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 
-# Create your models here.
-
-class User(AbstractBaseUser):
+class User(AbstractUser):
     username = models.CharField(unique=True, max_length=100)
     email = models.EmailField(unique=True)
-    full_name= models.CharField(unique=True, max_length=100)
-    otp = models.CharField(unique=True, max_length=100)
+    full_name = models.CharField(unique=True, max_length=100)
+    otp = models.CharField(max_length=100, null=True, blank=True)
+    refresh_token = models.CharField(max_length=1000, null=True, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
@@ -16,21 +16,21 @@ class User(AbstractBaseUser):
         return self.email
     
     def save(self, *args, **kwargs):
-        email_username, full_name = self.email.split('@')
+        email_username, full_name = self.email.split("@")
         if self.full_name == "" or self.full_name == None:
             self.full_name == email_username
-        
         if self.username == "" or self.username == None:
-            self.username == email_username
+            self.username = email_username
         super(User, self).save(*args, **kwargs)
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.FileField(upload_to='user_folder', default='default_user.jpg', null=True, blank=True)
-    country= models.CharField(max_length=100)
-    full_name= models.CharField(max_length=100, null=True, blank=True)
+    image = models.FileField(upload_to="user_folder", default="default-user.jpg", null=True, blank=True)
+    full_name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100, null=True, blank=True)
     about = models.TextField(null=True, blank=True)
-    date = models.DateField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         if self.full_name:
@@ -38,10 +38,19 @@ class Profile(models.Model):
         else:
             return str(self.user.full_name)
         
+    
     def save(self, *args, **kwargs):
         if self.full_name == "" or self.full_name == None:
             self.full_name == self.user.username
-            
         super(Profile, self).save(*args, **kwargs)
 
 
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
