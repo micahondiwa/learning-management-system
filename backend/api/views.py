ca7e7fe16f0fe6a4c9e23ecdac3d34bc11be4ac2
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-30 09:00:24 +0300

    JWT Auth

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,5 +3,5 @@ from api import serializers as api_serializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 # Create your views here.
-class MyTokenObtainPairSerializer(TokenObtainPairView):
+class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = api_serializer.MyTokenObtainPairSerializer
