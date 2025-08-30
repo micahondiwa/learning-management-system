029ba34ffd5730ee0f837b0c5752de2571db4a58
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-30 15:27:49 +0300

    MainWrapper in React

--- frontend/src/layout/MainWrapper.js
+++ frontend/src/layout/MainWrapper.js
@@ -16,5 +16,7 @@ const MainWrapper = ({ children }) => {
         handler();
     }, []);
 
-    return <>{loading ? null : children}</>
+    return <>{loading ? null : children}</>;
 };
+
+export default MainWrapper;
