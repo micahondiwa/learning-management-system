0525fb421c467a343a3435e095eeb687c0f6a0b7
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-30 09:08:33 +0300

    Registration serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -16,6 +16,15 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class RegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['full_name', 'email', 'password', 'password2']
+
+    def validate(self, attr):
+        if attr['password'] != attr['password2']:
+            raise serializers.ValidationError({"password": "password fields didn't match."})
 
 class UserSerializer(serializers.ModelSerializer):
 
