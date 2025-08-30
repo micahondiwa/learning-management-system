e23d1cd67d7c5a35aec2b8a8bef4877f3c408e4c
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-30 15:23:50 +0300

    MainWrapper in React

---
+++ frontend/src/layout/MainWrapper.js
@@ -0,0 +1,7 @@
+import { useEffect, useState } from 'react';
+import { setUser } from '../utils/auths'
+
+const MainWrapper = ({ children }) => {
+    const [loading, setLoading] = useState(true);
+
+}
