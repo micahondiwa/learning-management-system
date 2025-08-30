dff85fa1aee701e7ac08dd6abbc0955ee3d301b2
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-30 09:05:18 +0300

    Registration serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from userauths.models import Profile, User
@@ -12,6 +13,9 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token['username'] = user.username
 
         return token
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
 
 class UserSerializer(serializers.ModelSerializer):
 
