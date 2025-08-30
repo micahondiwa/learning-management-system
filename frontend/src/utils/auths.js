5ef5d29f55d4f7268046088811339589d40a1f74
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-30 14:27:45 +0300

    Authentication function

--- frontend/src/utils/auths.js
+++ frontend/src/utils/auths.js
@@ -4,6 +4,37 @@ import jwt_decode from 'jwt-decode';
 import Cookie from 'js-cookie';
 import Swal from 'sweetalert2';
 
-export const login = (email, password) = {
+export const login = async (email, password) => {
+    try {
+        const { data, status } = await axios.post('user/token/', {
+            email,
+            password,
+        })
 
+        if (status === 200) {
+            setAuthUser(data.access, data.refresh);
+            alert("Login Successful")
+        }
+        return { data, error: null }
+    } catch (error) {
+        return {
+            data: null,
+            eorr: error.response.data?.deatil || "Something went wrong",
+        }
+    };
+
+};
+
+export const register = async (full_name, email, password, password2) => {
+    try {
+        const { data } = await axios.post('user/register/', {
+            full_name,
+            email,
+            password,
+            password2,
+        })
+    } catch (error) {
+        console.log
+
+    }
 };
