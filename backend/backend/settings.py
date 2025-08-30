bacf05f99b8fd5e063d4770e7d4d98db7a6e00b2
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-30 08:04:16 +0300

    Custom apps

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -194,3 +194,5 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
