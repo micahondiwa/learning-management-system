7148c713f56bca85d7ff94cdca803ca442d45a59
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-30 08:06:08 +0300

    Custom apps

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -163,8 +163,8 @@ SIMPLE_JWT = {
     "UPDATE_LAST_LOGIN": False,
 
     "ALGORITHM": "HS256",
-    "SIGNING_KEY": settings.SECRET_KEY,
-    "VERIFYING_KEY": "",
+    # "SIGNING_KEY": settings.SECRET_KEY,
+    # "VERIFYING_KEY": "",
     "AUDIENCE": None,
     "ISSUER": None,
     "JSON_ENCODER": None,
