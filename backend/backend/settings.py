531437b777b30838238102782c971361ee880139
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-11-26 08:20:14 +0300

    API settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -98,7 +98,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
@@ -116,7 +115,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
