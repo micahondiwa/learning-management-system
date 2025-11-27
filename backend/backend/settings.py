39bf5e7965a728f6da5bbd3ba0bfbd72151f6d8e
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-11-27 07:21:15 +0300

    Static files dir

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_ROOTS = BASE_DIR / 'templates'
 
