b833e0ecb990c34cd3e5c089a7c05e89c9d66f97
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-12-15 07:28:04 +0300

    Constants

--- frontend/src/components/partials/BaseFooter.jsx
+++ frontend/src/components/partials/BaseFooter.jsx
@@ -61,6 +61,7 @@ function BaseFooter() {
           </div>
           <div className="offset-lg-1 col-lg-2 col-md-3 col-6">
             <div className="mb-4">
+
               {/* list */}
               <h3 className="fw-bold mb-3">Company</h3>
               <ul className="list-unstyled nav nav-footer flex-column nav-x-0">
